779a4f0a1cb8800eb9792147f053febeed846e20
Alyza Diaz Rodriguez <diazrodrigueza@wit.edu>
2022-04-21 17:45:52 -0400

    new fonts, starter components, and css

--- front/konbini/src/app/app.module.ts
+++ front/konbini/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CategoryItemsComponent } from './pages/category-items/category-items.component';
+import { ItemComponent } from './pages/item/item.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { HeaderComponent } from './chunks/header/header.component';
+import { ImageButtonComponent } from './bits/image-button/image-button.component';
+import { TextButtonComponent } from './bits/text-button/text-button.component';
+import { TextFieldComponent } from './bits/text-field/text-field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    CategoryItemsComponent,
+    ItemComponent,
+    CartComponent,
+    HeaderComponent,
+    ImageButtonComponent,
+    TextButtonComponent,
+    TextFieldComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/konbini/src/app/bits/image-button/image-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-button',
+  templateUrl: './image-button.component.html',
+  styleUrls: ['./image-button.component.scss']
+})
+export class ImageButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
